026a431898dae1d0726c04fd80da5adaff98ed81
LeeGangMin <rkdals0301@naver.com>
2020-05-05 04:34:01 +0900

    라우터 직접 만들기

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+const Login = { template: "<div>Login Page</div>" };
+
+const routes = {
+  "/": App,
+  "/login": Login
+};
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  computed: {
+    vueComponent() {
+      return (
+        routes[window.location.pathname] || {
+          template: "<div>Page not found</div>"
+        }
+      );
+    }
+  },
+  render(h) {
+    return h(this.vueComponent);
+  }
+});
