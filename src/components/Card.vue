50c55887d3983091920f9dd5f68ee7cea859e658
LeeGangMin <rkdals0301@naver.com>
2020-05-09 11:18:22 +0900

    중첩 라우트

---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Card
+    <div>{{cid}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  watch: {
+    $route() {
+      this.cid = this.$route.params.cid;
+    }
+  },
+  data() {
+    return {
+      cid: 0
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
