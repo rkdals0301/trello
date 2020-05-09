3ede890d03bd8b2238aebd846fae13c12b093ca1
LeeGangMin <rkdals0301@naver.com>
2020-05-09 11:30:17 +0900

    데이터 불러오기

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
     Card
-    <div>{{cid}}</div>
+    <div v-if="loading">loading card ...</div>
+    <div v-else>{{cid}}</div>
   </div>
 </template>
 
 <script>
 export default {
   watch: {
-    $route() {
-      this.cid = this.$route.params.cid;
+    $route: {
+      handler: "fetchData",
+      immediate: true
     }
   },
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false
     };
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.cid = this.$route.params.cid;
+        this.loading = false;
+      }, 500);
+    }
   }
 };
 </script>
