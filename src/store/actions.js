9914337221b27aba835668886156e9cc09f1ca96
LeeGangMin <rkdals0301@naver.com>
2020-05-17 15:09:15 +0900

    카드 수정 - API 연동

--- src/store/actions.js
+++ src/store/actions.js
@@ -28,6 +28,11 @@ const actions = {
   },
   FETCH_CARD({ commit }, { id }) {
     return api.card.fetch(id).then(data => commit("SET_CARD", data.item));
+  },
+  UPDATE_CARD({ dispatch }, { id, title, description, pos, listId }) {
+    return api.card
+      .update(id, { title, description, pos, listId })
+      .then(() => dispatch("FETCH_BOARD", { id: state.board.id }));
   }
 };
 
